850e0da19033a1e964354a1114283c1e24dfde2f
tianshaojian <tianshaojian@autohome.com.cn>
2024-06-18 14:42:15 +0800

    调整定时任务触发时间

--- src/model/schedule/schedule.go
+++ src/model/schedule/schedule.go
@@ -36,7 +36,7 @@ func Init() error {
 		schedule.Init()
 
 		// 增加房间定时清理任务：每天凌晨1点执行，清理超过3天未使用的且未关闭的房间
-		processTime := time.Date(2024, 6, 18, 11, 50, 0, 0, time.Local)
+		processTime := time.Date(2024, 6, 18, 1, 0, 0, 0, time.Local)
 		desc := "每天凌晨1点执行,清理超过3天未使用的且未关闭的房间"
 		AddSchedule("清理房间", desc, room.ClearUnusedRooms, processTime, 24*3600, schedule.SCHEDULE_TYPE_INTERVAL)
 
